6b6ecc8cd44f517b7b394d4bb8f359f700aca5bf
Agustín Maffini <agustinmaffini99@gmail.com>
2025-02-01 11:55:37 -0300

    modificacion a la task para el filtro

--- include/commons.h
+++ include/commons.h
@@ -2,6 +2,7 @@
 #include "../Source/include/FreeRTOS.h"
 #include "../Source/include/task.h"
 #include "../Source/include/queue.h"
+#include <stdlib.h>
 
 /* Constants for random number generation */
 #define TEMP_MIN               12
--- src/filter.c
+++ src/filter.c
@@ -2,18 +2,14 @@
 
 void vLowPassFilterTask(void *pvParameters)
 {
-    int buffer[FILTER_WINDOW_SIZE] = {0};
-    int index = 0, sum = 0, value, filteredValue;
-    for(;;)
-    {
-        if (xQueueReceive(xSensorValueQueue, &value, portMAX_DELAY) == pdPASS)
-        {
-            sum -= buffer[index];
-            buffer[index] = value;
-            sum += value;
-            index = (index + 1) % FILTER_WINDOW_SIZE;
-            filteredValue = sum / FILTER_WINDOW_SIZE;
-            xQueueSend(xFilteredValueQueue, &filteredValue, portMAX_DELAY);
-        }
+   int filteredValue, value;
+   int sum = 0;
+
+   while(1) {
+    if(xQueueReceive(xSensorValueQueue, &value, portMAX_DELAY)==pdPASS) {
+        sum = sum - (sum/FILTER_WINDOW_SIZE) + value;
+        filteredValue = sum/FILTER_WINDOW_SIZE;
+        xQueueSend(xFilteredValueQueue, &filteredValue, portMAX_DELAY);
     }
+   }
 }
